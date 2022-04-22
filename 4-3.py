ce15c31f93289cc4cc5390d9c921b8275c8e3967
a18091986 <78740241+a18091986@users.noreply.github.com>
2022-04-23 02:50:48 +0300

    Update 4-3.py

--- 4-3.py
+++ 4-3.py
@@ -1,3 +1,5 @@
 #Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Решите задание в одну строку..
 
 print(f'\n{[el for el in range(20,241) if (el % 20 == 0) | (el % 21 == 0)]}')
+
+https://github.com/a18091986/pythonbase/blob/homework_4/4-4.py
